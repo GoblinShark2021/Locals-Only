20e8837a1c6429bb7aae62894579184ad5648013
mikloska <mikloska973@gmail.com>
2021-06-23 23:29:51 -0400

    added catch-all 404 route in server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,10 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
-​
 const app = express();
-​
 const PORT = 3000;
-​
 app.use(express.json());
-
 // Route Handlers
-
-​
 //Default Error Handler
 app.use((err, req, res, next) => {
   const defaultErr = {
@@ -22,7 +16,6 @@ app.use((err, req, res, next) => {
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
-​
 if (process.env.NODE_ENV === 'production') {
   // statically serve everything in the build folder on the route '/build'
   app.use('/build', express.static(path.join(__dirname, '../build')));
@@ -31,13 +24,10 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, '../index.html'));
   });
 }
-​
 // Catch-all to unknown routes (404)
-app.use((req, res) => res.sendStatus(404));
-​
+app.use((req,res) => res.status(404).send('not found'))
 //Start Server
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
-​
 module.exports = app;
