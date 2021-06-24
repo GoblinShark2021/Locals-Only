30f0a6c0c848b95d30912f358b402291b0656aab
mikloska <mikloska973@gmail.com>
2021-06-23 23:15:55 -0400

    finalized package.json, webpack.config and set up and tested our server

--- client/app.js
+++ client/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const App = () => {
-    <div>
-        <h1 Hello World/>
-    </div>
-}
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+)}
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,41 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: './client/index.js',
-  devtool: 'inline-source-map',
+  entry: ['./client/index.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Mom and Pop',
-      template: './index.html',
-    }),
-  ],
-
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-        }
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.s?[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        loader: 'file-loader',
-        options: {
-          name: 'images/[name].[ext]'
-        }
-      }
-      
-    ]
+    ],
   },
   devServer: {
+    historyApiFallback: true,
     publicPath: '/build',
-    hot:true,
     proxy: {
-      '/': 'http://localhost:3000',
-    }
-}
-}
-
-
+      '/user': {
+        target: 'http://localhost:3000',
+      },
+    },
+  },
+};
