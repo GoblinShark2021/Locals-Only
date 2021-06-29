9da27181f94d369c5870ab396f97af3bd955852a
Charlie Malave <malavecharles@gmail.com>
2021-06-28 21:56:36 -0400

    favorites page - version 1

--- client/app.js
+++ client/app.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import Map from './components/Map';
-import Login from './components/Login';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Map from "./components/Map";
+import Login from "./components/Login";
+import Favorites from "./components/Favorites";
 
 const App = () => {
-    return (
+  return (
     <Router>
-        <section>
-            <Nav/>
-            <Switch>
-                <Route path='/' exact component={Home}/>
-                <Route path='/map' component={Map}/>
-                <Route path='/login' exact component={Login}/>
-            </Switch>
-        </section>
+      <section>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/map" component={Map} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/favorites" exact component={Favorites} />
+        </Switch>
+      </section>
     </Router>
-)}
+  );
+};
 
 export default App;
